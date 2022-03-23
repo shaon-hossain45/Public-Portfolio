1f528fee8fbbafed5f854cb206c9af8901fd3d97
shaon-hossain45 <shaonhossain915@gmail.com>
2022-03-23 10:25:05 +0600

    updated something

---
+++ includes/class-public-portfolio-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/shaon-hossain45/
+ * @since      1.0.0
+ *
+ * @package    Public_Portfolio
+ * @subpackage Public_Portfolio/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Public_Portfolio
+ * @subpackage Public_Portfolio/includes
+ * @author     shaon <shaonhossain615@gmail.com>
+ */
+class Public_Portfolio_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
