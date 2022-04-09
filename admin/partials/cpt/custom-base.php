c31f1d4b87ff252bcfbecf6f9a5d93b8ce6ce557
shaon-hossain45 <shaonhossain915@gmail.com>
2022-04-09 21:06:36 +0600

    updated today

--- admin/partials/cpt/custom-base.php
+++ admin/partials/cpt/custom-base.php
@@ -23,6 +23,8 @@ if ( ! class_exists( 'CptBaseSetup' ) ) {
 		 */
 		public function __construct() {
 			add_action( 'init', array( $this, 'custom_post_type' ), 0 );
+			add_action( 'init', array( $this, 'public_category' ), 0 );
+			add_action( 'init', array( $this, 'public_tag' ), 0 );
 		}
 
 		// Register Custom Post Type
@@ -33,18 +35,18 @@ if ( ! class_exists( 'CptBaseSetup' ) ) {
 				'singular_name'         => _x( 'Public Portfolio', 'Post Type Singular Name', 'public-portfolio' ),
 				'menu_name'             => __( 'Public Portfolios', 'public-portfolio' ),
 				'name_admin_bar'        => __( 'Public Portfolio', 'public-portfolio' ),
-				'archives'              => __( 'Item Archives', 'public-portfolio' ),
-				'attributes'            => __( 'Item Attributes', 'public-portfolio' ),
-				'parent_item_colon'     => __( 'Parent Item:', 'public-portfolio' ),
-				'all_items'             => __( 'All Items', 'public-portfolio' ),
-				'add_new_item'          => __( 'Add New Item', 'public-portfolio' ),
+				'archives'              => __( 'Portfolio Archives', 'public-portfolio' ),
+				'attributes'            => __( 'Portfolio Attributes', 'public-portfolio' ),
+				'parent_item_colon'     => __( 'Parent Portfolio:', 'public-portfolio' ),
+				'all_items'             => __( 'All Portfolios', 'public-portfolio' ),
+				'add_new_item'          => __( 'Add New Portfolio', 'public-portfolio' ),
 				'add_new'               => __( 'Add New', 'public-portfolio' ),
-				'new_item'              => __( 'New Item', 'public-portfolio' ),
-				'edit_item'             => __( 'Edit Item', 'public-portfolio' ),
-				'update_item'           => __( 'Update Item', 'public-portfolio' ),
-				'view_item'             => __( 'View Item', 'public-portfolio' ),
-				'view_items'            => __( 'View Items', 'public-portfolio' ),
-				'search_items'          => __( 'Search Item', 'public-portfolio' ),
+				'new_item'              => __( 'New Portfolio', 'public-portfolio' ),
+				'edit_item'             => __( 'Edit Portfolio', 'public-portfolio' ),
+				'update_item'           => __( 'Update Portfolio', 'public-portfolio' ),
+				'view_item'             => __( 'View Portfolio', 'public-portfolio' ),
+				'view_items'            => __( 'View Portfolios', 'public-portfolio' ),
+				'search_items'          => __( 'Search Portfolio', 'public-portfolio' ),
 				'not_found'             => __( 'Not found', 'public-portfolio' ),
 				'not_found_in_trash'    => __( 'Not found in Trash', 'public-portfolio' ),
 				'featured_image'        => __( 'Featured Image', 'public-portfolio' ),
@@ -53,16 +55,17 @@ if ( ! class_exists( 'CptBaseSetup' ) ) {
 				'use_featured_image'    => __( 'Use as featured image', 'public-portfolio' ),
 				'insert_into_item'      => __( 'Insert into item', 'public-portfolio' ),
 				'uploaded_to_this_item' => __( 'Uploaded to this item', 'public-portfolio' ),
-				'items_list'            => __( 'Items list', 'public-portfolio' ),
-				'items_list_navigation' => __( 'Items list navigation', 'public-portfolio' ),
+				'items_list'            => __( 'Portfolios list', 'public-portfolio' ),
+				'items_list_navigation' => __( 'Portfolios list navigation', 'public-portfolio' ),
 				'filter_items_list'     => __( 'Filter items list', 'public-portfolio' ),
 			);
-			$args   = array(
+
+			$args = array(
 				'label'               => __( 'Public Portfolio', 'public-portfolio' ),
 				'description'         => __( 'Post Type Description', 'public-portfolio' ),
 				'labels'              => $labels,
 				'supports'            => array( 'title', 'editor', 'thumbnail' ),
-				'taxonomies'          => array( 'category', 'post_tag' ),
+				'taxonomies'          => array( 'public_category', 'public_tag' ),
 				'hierarchical'        => false,
 				'public'              => true,
 				'show_ui'             => true,
@@ -80,5 +83,84 @@ if ( ! class_exists( 'CptBaseSetup' ) ) {
 			register_post_type( 'public_portfolio', $args );
 
 		}
+
+
+		// Register Custom Taxonomy
+		function public_category() {
+
+			$labels = array(
+				'name'                       => _x( 'Public Categories', 'Taxonomy General Name', 'public-portfolio' ),
+				'singular_name'              => _x( 'Public Category', 'Taxonomy Singular Name', 'public-portfolio' ),
+				'menu_name'                  => __( 'Public Category', 'public-portfolio' ),
+				'all_items'                  => __( 'All Items', 'public-portfolio' ),
+				'parent_item'                => __( 'Parent Category', 'public-portfolio' ),
+				'parent_item_colon'          => __( 'Parent Category:', 'public-portfolio' ),
+				'new_item_name'              => __( 'New Category Name', 'public-portfolio' ),
+				'add_new_item'               => __( 'Add New Category', 'public-portfolio' ),
+				'edit_item'                  => __( 'Edit Category', 'public-portfolio' ),
+				'update_item'                => __( 'Update Category', 'public-portfolio' ),
+				'view_item'                  => __( 'View Category', 'public-portfolio' ),
+				'separate_items_with_commas' => __( 'Separate items with commas', 'public-portfolio' ),
+				'add_or_remove_items'        => __( 'Add or remove items', 'public-portfolio' ),
+				'choose_from_most_used'      => __( 'Choose from the most used', 'public-portfolio' ),
+				'popular_items'              => __( 'Popular Categories', 'public-portfolio' ),
+				'search_items'               => __( 'Search Categories', 'public-portfolio' ),
+				'not_found'                  => __( 'Not Found', 'public-portfolio' ),
+				'no_terms'                   => __( 'No items', 'public-portfolio' ),
+				'items_list'                 => __( 'Categories list', 'public-portfolio' ),
+				'items_list_navigation'      => __( 'Categories list navigation', 'public-portfolio' ),
+			);
+			$args   = array(
+				'labels'            => $labels,
+				'hierarchical'      => true,
+				'public'            => true,
+				'show_ui'           => true,
+				'show_admin_column' => true,
+				'show_in_nav_menus' => true,
+				'show_tagcloud'     => true,
+			);
+			register_taxonomy( 'public_category', array( 'public_portfolio' ), $args );
+
+		}
+
+
+		// Register Custom Taxonomy
+		function public_tag() {
+
+			$labels = array(
+				'name'                       => _x( 'Public Tags', 'Taxonomy General Name', 'public-portfolio' ),
+				'singular_name'              => _x( 'Public Tag', 'Taxonomy Singular Name', 'public-portfolio' ),
+				'menu_name'                  => __( 'Public Tag', 'public-portfolio' ),
+				'all_items'                  => __( 'All Tags', 'public-portfolio' ),
+				'parent_item'                => __( 'Parent Tag', 'public-portfolio' ),
+				'parent_item_colon'          => __( 'Parent Tag:', 'public-portfolio' ),
+				'new_item_name'              => __( 'Public Tag', 'public-portfolio' ),
+				'add_new_item'               => __( 'Add New Tag', 'public-portfolio' ),
+				'edit_item'                  => __( 'Edit Tag', 'public-portfolio' ),
+				'update_item'                => __( 'Update Tag', 'public-portfolio' ),
+				'view_item'                  => __( 'View Tag', 'public-portfolio' ),
+				'separate_items_with_commas' => __( 'Separate items with commas', 'public-portfolio' ),
+				'add_or_remove_items'        => __( 'Add or remove items', 'public-portfolio' ),
+				'choose_from_most_used'      => __( 'Choose from the most used', 'public-portfolio' ),
+				'popular_items'              => __( 'Popular Tags', 'public-portfolio' ),
+				'search_items'               => __( 'Search Tags', 'public-portfolio' ),
+				'not_found'                  => __( 'Not Found', 'public-portfolio' ),
+				'no_terms'                   => __( 'No items', 'public-portfolio' ),
+				'items_list'                 => __( 'Tags list', 'public-portfolio' ),
+				'items_list_navigation'      => __( 'Tags list navigation', 'public-portfolio' ),
+			);
+			$args   = array(
+				'labels'            => $labels,
+				'hierarchical'      => false,
+				'public'            => true,
+				'show_ui'           => true,
+				'show_admin_column' => true,
+				'show_in_nav_menus' => true,
+				'show_tagcloud'     => true,
+			);
+			register_taxonomy( 'public_tag', array( 'public_portfolio' ), $args );
+
+		}
+
 	}
 }
--- public/partials/output/element-box.php
+++ public/partials/output/element-box.php
@@ -38,16 +38,66 @@ if ( ! class_exists( 'outputBoxSetup' ) ) {
 		 */
 		public function shortcode_boxed( $atts, $content = null, $tag = '' ) {
 			global $post;
-			$a = shortcode_atts(
-				array(
-					'title'       => 'Title',
-					'title_color' => 'white',
-					'color'       => 'blue',
-				),
-				$atts
-			);
 
-			echo $output = 'shaon';
+			$output = '';
+			$output .= '<div id="acx_webex_body_content">
+            <div class="section-custom common_padding">
+                <div class="acx_inside">
+                    <div class="acx_common">';
+
+                    $output .= '<div class="acx_gallery_filtr ">
+					<div class="acx_gal_filter_grp filters-button-group">
+                        <a class="acx_button is-checked" data-filter="*">all works</a>';
+                        $tax_terms = get_terms( array(
+                            'taxonomy' => 'public_category',
+                            'orderby'    => 'count',
+                            'hide_empty' => true,
+                            'order'		=>'ASC'
+                        ));
+						<a class="acx_button" data-filter=".ok">ok</a>
+						<a class="acx_button" data-filter=".ikll">ikll</a>
+
+                        $output .= '</div> <!-- acx_gal_filter_grp-->
+				</div> <!-- acx_gallery_filtr -->';
+
+                $output .= '<div class="acx_gallery_wrk_cnvs">
+					<div class="acx_gal_wrk_split element-item ok" data-category="ok" style="position: absolute; left: 0px; top: 0px;">
+                        <div class="acx_gal_inner_split"><img src="http://localhost/wordpress-local/wp-content/uploads/2022/04/shutterstock_547563823-1024x1024-1.jpg"><span class="hover_bg">
+                        <div class="gal_hvr_cntnt">
+                        <div class="gal_link_hvr">
+                            <a onclick="acx_w_portfolio_opened(93);" class="gal_zoom"></a><a href="gfgf" class="gal_linkto"></a></div> <!-- gal_link_hvr -->
+                        <span class="gal_hvr_ttl"></span>
+                        </div> <!-- gal_hvr_cntnt-->
+                        </span>
+                        </div> <!-- acx_gal_inner_split-->
+                    </div> <!-- acx_gal_wrk_split-->
+					<div class="acx_gal_wrk_split element-item ok" data-category="ikll" style="position: absolute; left: 159.969px; top: 0px;">
+                        <div class="acx_gal_inner_split"><img src="http://localhost/wordpress-local/wp-content/uploads/2022/04/shutterstock_547563823-1024x1024-1.jpg"><span class="hover_bg">
+                        <div class="gal_hvr_cntnt">
+                        <div class="gal_link_hvr">
+                            <a onclick="acx_w_portfolio_opened(92);" class="gal_zoom"></a><a href="jhjhj" class="gal_linkto"></a></div> <!-- gal_link_hvr -->
+                        <span class="gal_hvr_ttl"></span>
+                        </div> <!-- gal_hvr_cntnt-->
+                        </span>
+                        </div> <!-- acx_gal_inner_split-->
+                    </div> <!-- acx_gal_wrk_split-->
+					<div class="acx_gal_wrk_split element-item ikll" data-category="ikll" style="position: absolute; left: 319.938px; top: 0px;">
+                        <div class="acx_gal_inner_split"><img src="http://localhost/wordpress-local/wp-content/uploads/2022/04/Background.jpg"><span class="hover_bg">
+                        <div class="gal_hvr_cntnt">
+                        <div class="gal_link_hvr">
+                            <a onclick="acx_w_portfolio_opened(89);" class="gal_zoom"></a><a href="popop" class="gal_linkto"></a></div> <!-- gal_link_hvr -->
+                        <span class="gal_hvr_ttl"></span>
+                        </div> <!-- gal_hvr_cntnt-->
+                        </span>
+                        </div> <!-- acx_gal_inner_split-->
+                    </div> <!-- acx_gal_wrk_split-->
+				</div> <!-- acx_gallery_wrk_cnvs -->';
+
+                    $output .= '</div><!-- acx_common -->
+        </div><!-- acx_inside -->
+    </div><!-- section -->
+</div>';
+			return $output;
 
 		}
 	}
